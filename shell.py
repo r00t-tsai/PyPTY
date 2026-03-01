4474df7dd631c45f3f5194862619cca271c459b8
zhai <256692333+r00t-tsai@users.noreply.github.com>
2026-03-01 21:10:55 +0800

    shell.py

--- shell.py
+++ shell.py
@@ -1,17 +1,20 @@
 import sys
+import signal
 from interpreter.interpreter import Shell
+
 
 def main():
     interpreter = Shell()
+    old_sigint = signal.signal(signal.SIGINT, signal.SIG_IGN)
     try:
         interpreter._run()
-    except KeyboardInterrupt:
-        pass
     finally:
+        signal.signal(signal.SIGINT, old_sigint)
         interpreter.cleanup()
-      
+
+
 if __name__ == "__main__":
     if sys.platform != "win32":
-        print("Error: This is build is available only on Windows.")
+        print("Error: ConPTY is only available on Windows.")
         sys.exit(1)
     main()
